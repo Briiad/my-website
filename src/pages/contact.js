a2149c265ea46d53296d6a35a798d5b371470699
Brian Aditya <brianadit33@gmail.com>
2021-03-14 22:56:02 +0800

    init

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Contact = () => (
+    <Layout>
+        <SEO title=" brayeeen | contact" />
+        <h1>Contact</h1>
+    </Layout>
+)
+
+export default Contact
